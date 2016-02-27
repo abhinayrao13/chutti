17c3821bf1f4f2b2b1c3679331ae06202d578c3f
sampathmanikanta <sampathmanikanta8@gmail.com>
2016-02-27 12:16:54 +0530

    added js and css files required  for admin lte template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require roles
+//= require app.min
+//= require bootstrap.min
---
+++ app/assets/javascripts/roles.js
@@ -0,0 +1,21 @@
+function to_check(){
+  var a = document.getElementById("user_role");
+  var letters = /^[A-Za-z]+$/;
+  if (a.value == "") {
+    alert("pls fill the feilds");
+    return false;
+  }
+  else if(!a.value.match(letters)){
+    alert("enter only letters");
+    return false;
+  }
+}
+
+function my_btn(l) {
+  $.ajax({ url: "/roles/"+ l, method: "delete", datatype: "json",
+  success: function (data) {
+    console.log(data);
+    $("#role_" + data.id).remove();
+  }
+ });
+}
