ca5820a5993c3a6a5bda1c8e83ccfb7f842d5a02
Abhinay Rao <abhinayrao13@gmail.com>
2016-03-11 12:07:37 +0530

    changed order in which timeline is displayed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,9 @@ class DashboardController < ApplicationController
   before_action :admin
   def index
     @pending_leaves = Leave.where(status: "pending").reverse
-    @desc_checkins = Checkin.all
+    @desc_checkins = Checkin.order(date: :desc)
     @users = User.where(:id => @desc_checkins.collect{|x| x.user_id}).collect{|x| {x.id => {url: x.image.url, name: x.name}}}.reduce( Hash.new, :merge)
-    @desc_checkins = [@desc_checkins.as_json.each{|x| x["type"] = "check_in"} + @desc_checkins.select{|x| x.check_out != nil}.as_json.each{|x| x["type"] = "check_out"}].flatten.sort{|x,y| x["date"] <=> y["date"]}
+    @desc_checkins = [@desc_checkins.as_json.each{|x| x["type"] = "check_in"} + @desc_checkins.select{|x| x.check_out != nil}.as_json.each{|x| x["type"] = "check_out"}].flatten.sort{|x,y| x["date"] <=> y["date"]}.reverse
     @desc_checkins = @desc_checkins.as_json
     @before_paginate_checkins = @desc_checkins
     @desc_checkins = Kaminari.paginate_array(@desc_checkins).page(params[:page]).per(3)
@@ -14,5 +14,5 @@ class DashboardController < ApplicationController
     #p @before_paginate_checkins[@index - 1]["date"] == @before_paginate_checkins[@index]["date"]
     @current_date_rendered = @before_paginate_checkins[@index - 1]["date"] == @before_paginate_checkins[@index]["date"]
   end
-  
+
 end
